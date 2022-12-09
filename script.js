d92b98343fee83f9bbe6d638c9864cf31fe4e89c
xxxxxyz <92290793+xxxxxyz@users.noreply.github.com>
2022-12-10 00:16:34 +1100

    Polish Home Page Styling (#4)
    * Home Page Styling -Part 1
    
    * zoe/MPP-16-Part2
    
    * Home Page Styling Done
    
    * Remove unncessary space from the index.html

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ $(document).ready(function() {
             $('.navbar').addClass("sticky");
         }else {
             $('.navbar').removeClass("sticky");
+        };
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
-
+    // slide-up Script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
     });
+    
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["a Front-end developer.", "Nice to see you!", "Have a Great Day!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: false
+    })
 })
