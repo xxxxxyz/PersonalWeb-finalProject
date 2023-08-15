bd9fdbb83e94ffee92d7a08d2fce247f84b39afe
xxxxxyz <92290793+xxxxxyz@users.noreply.github.com>
2023-08-15 22:34:42 +1000

    Carousel fix (#22)
    * Fix Issues
    
    * Fix animation

--- portfolio/src/components/Projects/ProjectCard/ProjectCard.jsx
+++ portfolio/src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -8,10 +8,13 @@ const ProjectCard = (props) => {
     <Box className='card-container' sx={{backgroundImage: `url(${props.imageUrl})`}}>
       <Box className='card-content-container'>
         <Typography variant='h5' className='card-title' gutterBottom>{props.title}</Typography>
-          <Typography variant='body2' color='text.secondary' className='card-content' sx={{display: 'flex', justifyContent:'space-between'}}gutterBottom>{props.content} <a href='/projects/projectpage' style={{fontFamily: 'Outfit', textDecoration: 'underline', color: '#fff',fontWeight: '300', fontSize: '10px'}}>More</a></Typography>
-          
-      </Box>
-        
+          <Typography variant='body2' color='text.secondary' className='card-content' sx={{display: 'flex', justifyContent:'space-between'}} gutterBottom >
+            {props.content}
+            <a href='/projects/projectpage' style={{fontFamily: 'Outfit', textDecoration: 'underline', color: '#fff',fontWeight: '300', fontSize: '10px'}}>
+              More
+            </a>
+          </Typography>         
+      </Box>        
     </Box>
   )
 }
