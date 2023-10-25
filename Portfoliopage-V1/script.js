1626563f49567dc6ac181f8947f980869f6e533b
yuzexia <92290793+xxxxxyz@users.noreply.github.com>
2023-10-25 11:49:54 +1100

    Organize the Repo

---
+++ Portfoliopage-V1/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        }else {
+            $('.navbar').removeClass("sticky");
+        };
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up Script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+    });
+    
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["a Front-end developer.", "Nice to see you!", "Have a Great Day!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: false
+    })
+})
