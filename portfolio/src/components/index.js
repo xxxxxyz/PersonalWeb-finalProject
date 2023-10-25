fa2cc25b277a1a4970fde5588f96a6684f3c6428
yuzexia <92290793+xxxxxyz@users.noreply.github.com>
2023-10-25 11:55:41 +1100

    Organize the repo

--- portfolio/src/components/index.js
+++
@@ -1,6 +0,0 @@
-
-export { default as HomePage } from './HomePage/HomePage';
-export { default as Navbar } from './Navbar/Navbar';
-export { default as BottomBar } from './BottomBar/BottomBar';
-export { default as Project } from './Project/Project';
-export { default as ProjectsPage } from './ProjectsPage/ProjectsPage';
