63ceb179d53d4d35beac68e8a0adc28dfcbb2733
yuzexia <92290793+xxxxxyz@users.noreply.github.com>
2023-11-02 23:56:06 +1100

    First implementation on the fade in up animation

--- portfolio_v1/index.js
+++ portfolio_v1/index.js
@@ -1,1 +1,16 @@
+// parallax fade in up effect on scrolling the window 
+const project_1 = document.querySelector('#project_1')
+const project_1_position = project_1.offsetTop
 
+const project_1_content = document.querySelector('#project_1_content')
+
+
+// console.log(project_1_position);
+/* window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    if(window.scrollY >= project_1_position) {
+        // project_1_content.classList.add("animate-fade-in-up")
+    } else {
+        // project_1_content.classList.remove("animate-fade-in-up")
+    }
+}) */
--- portfolio_v1/tailwind.config.js
+++ portfolio_v1/tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      keyframes: {
+        wiggle:{
+          '0%,100%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+        fadeInUp:{
+          'from': {
+            transform: 'translateY(20px)',
+            opacity:'0',
+          },
+          'to': {
+            transform: 'translateY(0)',
+            opacity:'100',
+          },
+        },
+      },
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+        'fade-in-up': 'fadeInUp 0.75 ease-in-out',
+      },
     },
   },
   plugins: [],
