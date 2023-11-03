39fe2ea224b505ec949835a357a244e4a956c0c5
xxxxxyz <92290793+xxxxxyz@users.noreply.github.com>
2023-11-03 15:51:41 +1100

    Fade in up animation (#26)
    * First implementation on the fade in up animation
    
    * Add fade up animation on scroll

--- portfolio_v1/index.js
+++ portfolio_v1/index.js
@@ -1,1 +1,32 @@
+// parallax fade in up effect on scrolling the window 
 
+/* let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        const content = entry.target.querySelector('#project_1_content')
+        if (entry.isIntersecting) {
+            if(!content.classList.contains('animate-fade-in-up')) {
+                content.classList.add('animate-fade-in-up')
+                console.log('2');
+            } else {
+                content.classList.remove('animate-fade-in-up')
+                console.log('3');
+            }
+        }  
+        
+        content.classList.remove('animate-fade-in-up')
+    })
+})
+
+observer.observe(document.querySelector('#project_1'))
+
+const button = document.querySelector('#contact_btn')
+const title = document.querySelector('#project_1_content')
+button.addEventListener('click', () => {    
+    if(!title.classList.contains('animate-fade-in-up')) {
+        title.classList.add('animate-fade-in-up')
+        console.log('2');
+    } else {
+        title.classList.remove('animate-fade-in-up')
+        console.log('3');
+    }
+}) */
--- portfolio_v1/tailwind.config.js
+++ portfolio_v1/tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      keyframes: {
+        wiggle:{
+          '0%,100%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+        fadeInUp:{
+          'from': {
+            '--webkit0transform': 'translateY(50px)',
+            transform: 'translateY(50px)',
+            opacity:'0',
+          },
+          'to': {
+            '--webkit0transform': 'translateY(0)',
+            transform: 'translateY(0)',
+            opacity:'1',
+          },
+        },
+      },
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+        'fade-in-up': 'fadeInUp 0.75s ease-in-out',
+      },
     },
   },
   plugins: [],
 };
+
